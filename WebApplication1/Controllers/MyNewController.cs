291ef6bd556429f0edbe28de5ea191047340f709
Hoang Quoc Anh <hqanhh7605@gmail.com>
2025-08-22 20:22:18 +0700

    add file

---
+++ WebApplication1/Controllers/MyNewController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class MyNewController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "Hoang Quoc Anh";
+            ViewBag.Age = 20;
+            TempData["Email"] = "hqanhh7605@gmai.com";
+            return RedirectToAction("Sample");
+        }
+        public IActionResult Sample()
+        {
+            return View("Index"); // return ve action cua Index
+           
+        }
+
+        public String Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
