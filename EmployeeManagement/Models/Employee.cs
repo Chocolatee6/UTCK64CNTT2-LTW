06120cf2500a090c74fb84bd55552ac15abe0ab3
Hoang Quoc Anh <hqanhh7605@gmail.com>
2025-09-23 17:48:25 +0700

    update file

---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Full Name")]
+        public string fullName { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        
+        public decimal Salary { get; set; }
+        [Required]
+        public string Status { get; set; }
+    }
+}
