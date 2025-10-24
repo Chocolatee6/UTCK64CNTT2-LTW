9b522b71610041fddc22f3b7cb9cf70896ac1f68
Hoang Quoc Anh <hqanhh7605@gmail.com>
2025-10-24 12:09:48 +0700

    xoa

--- hqa_231230703_de01/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace hqa_231230703_de01.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
