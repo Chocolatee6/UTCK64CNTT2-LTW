a88268249192de3eaedc4cf75cb25d9c5b201bb4
Hoang Quoc Anh <hqanhh7605@gmail.com>
2025-10-24 12:08:45 +0700

    xóa folder

--- HoangQuocAnh_231230703_de02/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HoangQuocAnh_231230703_de02.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
