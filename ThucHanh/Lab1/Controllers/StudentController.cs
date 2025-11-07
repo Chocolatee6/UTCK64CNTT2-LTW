7c11a8caf960b09ae89085db7a85eb4587c75a04
Hoang Quoc Anh <hqanhh7605@gmail.com>
2025-11-07 20:56:03 +0700

    ThucHanh1,2

---
+++ ThucHanh/Lab1/Controllers/StudentController.cs
@@ -0,0 +1,54 @@
+﻿using Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Lab1.Controllers
+{
+    public class StudentController : Controller
+    {
+        private List<Student> listudent = new List<Student>();
+        public StudentController() {
+            listudent = new List<Student>()
+            {
+                new Student()
+                {
+                    Id = 101, Name = "Hoàng Quốc Anh", Branch = Branch.IT, Gender = Gender.Male, IsRegular = true,Address = "Nam Định",Email = "hqanh7605@gmail.com"
+                }
+
+
+
+            };
+            
+        }
+
+
+        public IActionResult Index()
+        {
+            return View(listudent);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.AllGenders = Enum.GetValues(typeof(Gender)).Cast<Gender>().ToList();
+            ViewBag.AllBranches = new List<SelectListItem>()
+            { new SelectListItem { Text = "IT", Value  = "1"},
+            new SelectListItem { Text = "BE", Value  = "2"},
+            new SelectListItem { Text = "CE", Value  = "3"},
+            new SelectListItem { Text = "EE", Value  = "4"},
+
+
+
+            };
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Student student)
+        {
+            student.Id = listudent.Last<Student>().Id+1;
+            listudent.Add(student);
+            return View("Index",listudent);
+        }
+    }
+}
---
+++ ThucHanh/Lab1/wwwroot/js/mylayout.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    console.log('MyLayout script loaded successfully.');
+
+    // Thêm chức năng cơ bản cho Header (ví dụ: đổi màu khi cuộn)
+    var header = document.querySelector('.main-header');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 50) {
+            header.style.backgroundColor = '#2c3e50'; // Màu tối hơn khi cuộn
+        } else {
+            header.style.backgroundColor = '#34495e'; // Màu ban đầu
+        }
+    });
+
+    // Ví dụ: Thêm sự kiện click cho sidebar
+    var sidebarItems = document.querySelectorAll('.sidebar li');
+    sidebarItems.forEach(function (item) {
+        item.addEventListener('click', function () {
+            alert('Clicked on: ' + this.textContent);
+        });
+    });
+});
